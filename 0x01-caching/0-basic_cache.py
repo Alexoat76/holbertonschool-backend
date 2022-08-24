857bc5b36e5e46f509ba3a931cfd593fe4952e09
Alexoat76 <3915@holbertonschool.com>
2022-08-24 09:22:38 -0500

    Class BasicCache | Inherits from BaseCaching and | Caching system :heavy_check_mark:

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,16 +12,23 @@ class BasicCache(BaseCaching):
     This class is a caching system that stores data in a dictionary.
     """
 
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+
     def put(self, key, item):
         """
-        Adds an item to the cache.
+        Adds an item to the cache and updates the cache.
         """
+        if not key or not item:
+            return
         self.cache_data[key] = item
 
     def get(self, key):
         """
         Returns an item from the cache.
         """
-        if key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        if key is None or key not in self.cache_data:
+            return
+        return self.cache_data[key]
