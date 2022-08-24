54611276c966a8f1f5dbf2d3eeea14c0324f94e0
Alexoat76 <3915@holbertonschool.com>
2022-08-24 09:58:17 -0500

    Class LIFOCache | Inherits from BaseCaching and Caching system :heavy_check_mark:

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -20,11 +20,11 @@ class LIFOCache(BaseCaching):
             self.cache_data[key] = item  # Add item in the cache
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             # if the cache is full, remove the last item added
-            self.cache_data.pop(self.last_put)  # remove the last item added
-            print("DISCARD: ", self.last_put)  # print the last item discarded
+            self.cache_data.pop(self.last_item)  # remove the last item added
+            print("DISCARD: ", self.last_item)  # print the last item discarded
         if key:
             # if the key is not empty, set the last_put to the key added
-            self.last_put = key
+            self.last_item = key
 
     def get(self, key):
         """ Get an item from the cache
