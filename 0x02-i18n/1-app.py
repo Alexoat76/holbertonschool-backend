940a5c7901f166ce0879817e11d0b8a2cbe6bcf0
Alexoat76 <3915@holbertonschool.com>
2022-08-30 15:24:55 -0500

    Create config languages class | :heavy_check_mark:

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Script that starts a basic flask Babel setup
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route('/', methods=["GET"], strict_slashes=False)
+def index():
+    """GET /
+    Return: The render of 1-index.html"""
+    return render_template('1-index.html')
+
+
+class Config(object):
+    """flask app config Class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Set the above class object as the configuration for the app
+app.config.from_object('1-app.Config')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
