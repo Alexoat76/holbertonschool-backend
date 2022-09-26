37c9df3fde5e062424753d6a50f9f4a5c3bae469
Alexoat76 <3915@holbertonschool.com>
2022-09-26 16:54:24 -0500

    Store and Hash Operations :heavy_check_mark:

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,19 +1,19 @@
 import redis from 'redis';
 
 const client = redis.createClient();
-
+// client.on('error', (err) => { console.log('Error ' + err); }); // error handler
 client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`));
 client.on('connect', () => console.log('Redis client connected to the server'));
 
-const KEY = 'HolbertonSchools';
+const KEY = 'HolbertonSchools'; // key to store the list of schools in Redis
 
 const keys = ['Portland', 'Seattle', 'New York', 'Bogota', 'Cali', 'Paris'];
 const values = [50, 80, 20, 20, 40, 2];
 
-keys.forEach((key, index) => {
+keys.forEach((key, index) => { // store the list of schools in Redis
   client.hset(KEY, key, values[index], redis.print);
 });
 
-client.hgetall(KEY, (err, value) => {
+client.hgetall(KEY, (err, value) => { // print the list of schools stored in Redis
   console.log(value);
 });
